f7fdccc0f33698be7fe422471b696786b65df38f
JohnOliverMoura <mouraxjohn@gmail.com>
2025-03-23 21:22:54 +0000

    Desafio Aventureiro

--- xadrez.c
+++ xadrez.c
@@ -4,53 +4,52 @@
 
 
 int main() {
-       //Declarando a variavel.
-       int R = 1, T = 1, B = 1, C = 1; 
+    //Declarando a variavel.
+    int R = 1, T = 1, B = 1, C = 1; 
 
-       printf("\nRainha se movimenta\n");
+    printf("\nRainha se movimenta\n");
    
-       //Usando a estrutura de repetição while para a movimentação da Rainha 
-       while (R <= 8)
-       {
-           printf(" Esquerda\n");
-           R++;
-       }
+    //Usando a estrutura de repetição while para a movimentação da Rainha 
+    while (R <= 8)
+    {
+        printf(" Esquerda\n");
+        R++;
+    }
        
    
-       printf("\nBispo se movimenta\n");
+    printf("\nBispo se movimenta\n");
    
-       //Usando a estrutura de repetição do- while para a movimentação do Dispo 
-       do
-       {
-           printf(" Cima -  Direita\n");
-           B++;
-       } while (B <=5);
+    //Usando a estrutura de repetição do- while para a movimentação do Dispo 
+    do
+    {
+        printf(" Cima -  Direita\n");
+        B++;
+    } while (B <=5);
    
    
-       printf("\nTorre se movimenta\n");
+    printf("\nTorre se movimenta\n");
    
-       //Usando a estrutura de repetição for para movimentação da Torre
+    //Usando a estrutura de repetição for para movimentação da Torre
    
-       for (T; T <= 5; T++)
-       {
-           printf(" Direita\n");
-       }
+    for (T; T <= 5; T++)
+    {
+        printf(" Direita\n");
+    }
 
-   //Utilizando estrutura de repetição aninhada (while e for) para a movimentação do Cavalo
+    //Utilizando estrutura de repetição aninhada (while e for) para a movimentação do Cavalo
 
-   printf("\n-Cavalo se movimenta \n");
+    printf("\nCavalo se movimenta \n");
 
-   int movimento = 1; // declaração da variavel de movimento do cavalo para garantir o movimento em "L"
+    int movimento = 1; // declaração da variavel de movimento do cavalo para garantir o movimento em "L"
 
-   while (movimento --) //O "--" garante que a condição só será verdadeira no primeiro teste. 
-   {
+    while (movimento --) //O "--" garante que a condição só será verdadeira no primeiro teste. 
+    {
        for (C; C < 3; C++)
        {
-           printf("Baixo \n");
+           printf(" Baixo \n");
        }
-       printf("Esquerda \n");
-   }
-
+       printf(" Esquerda \n");
+    }
 
 
 
